14958755e28fea2476a685206230e54240b151de
Minh Nhut <lenhut0929@gmail.com>
2024-07-21 18:32:34 +0700

    Day 1: Coding Convention
    Setup Java project,
    Write first project

---
+++ java-01/src/vn/edu/likelion/day1/HelloWorld.java
@@ -0,0 +1,15 @@
+package vn.edu.likelion.day1;
+
+public class HelloWorld {
+    /*
+   Author: Tác giả
+   Date: 17/06/2024
+   Function: Ham chinh, chay dau tien
+   Return: Khong trả về gì cả
+    */
+    public static void main(String[] args) {
+        // comment 1 dòng
+        // print, printf, println
+        System.out.println("Hello World!");
+    }
+}
